64e597f81361b03305a4b6cdf3e8f7aa3bb80bb9
Mark James Espinosa <jamesespinosamark.com>
2026-04-19 21:41:24 +0800

    Add: Layout (Main shop page)

---
+++ app/layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="site-wrap">
+    <TheNav />
+    <main class="site-main"><slot /></main>
+    
+  </div>
+</template>
+<style scoped>
+.site-wrap { display:flex; flex-direction:column; min-height:100vh; }
+.site-main  { flex:1; padding-top:var(--nav-h); }
+</style>
